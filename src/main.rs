4561108d3546933c1c526fd5140d633c6b4db204
Kit Peters <christopher.peters@cbsinteractive.com>
2020-12-16 12:33:35 -0600

    It works!

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
-//! Default Compute@Edge template program.
-extern crate log_fastly;
 mod spacex_tle;
+
+use config::{Config, FileFormat};
 use fastly::http::{Method, StatusCode};
 use fastly::{Body, Error, Request, Response, ResponseExt};
 use serde_json::json;
 use spacex_tle::SpacexTLE;
 
-const LOGGING_ENDPOINT: &str = "stdout";
+const LOGGING_ENDPOINT: &str = "get_test_syslog";
 const N2YO_API_KEY: &str = "YBLNQJ-JUG3KB-XS5BRT-1JX2";
-
-const TXN_LIMIT: i32 = 6;
+const TXN_LIMIT: i64 = 6;
 
 /// The entry point for your application.
 ///
@@ -19,11 +18,10 @@ const TXN_LIMIT: i32 = 6;
 ///
 /// If `main` returns an error, a 500 error response will be delivered to the client.
 #[fastly::main]
-fn main(req: Request<Body>) -> Result<impl ResponseExt, Error> {
+fn main(req: Request<Body>) -> Result<Response<Body>, Error> {
     logging_init();
-    log::debug!("*******************************************************");
     log::debug!("Request: {} {}", req.method(), req.uri());
-    
+
     // We can filter requests that have unexpected methods.
     const VALID_METHODS: [Method; 1] = [Method::GET];
     if !(VALID_METHODS.contains(req.method())) {
@@ -31,29 +29,64 @@ fn main(req: Request<Body>) -> Result<impl ResponseExt, Error> {
             .status(StatusCode::METHOD_NOT_ALLOWED)
             .body(Body::from("This method is not allowed"))?);
     }
-   
+
     let url_parts = &req.uri().path().split("/").collect::<Vec<&str>>()[1..];
     if url_parts[0] == "tle" {
         let mission_id = url_parts[1];
         let mut spacex_tle = SpacexTLE::new(N2YO_API_KEY, TXN_LIMIT);
-        let payload_tles = spacex_tle.payload_tles(mission_id);
+        let payload_tles = spacex_tle.payload_tles(mission_id)?;
         if payload_tles.is_some() {
             let json = json!(payload_tles);
-            return Ok(Response::builder().status(StatusCode::OK).header("Content-Type", "application/json").body(Body::from(format!("{}",json))).unwrap());
-        } else { 
-           return Ok(Response::builder().status(StatusCode::NOT_FOUND).body(Body::from(format!("No TLEs found for mission {}", mission_id))).unwrap()); 
-        } 
+            return Ok(Response::builder()
+                .status(StatusCode::OK)
+                .header("Content-Type", "application/json")
+                .body(Body::from(format!("{}", json)))
+                .unwrap());
+        } else {
+            return Ok(Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(Body::from(format!(
+                    "No TLEs found for mission {}",
+                    mission_id
+                )))
+                .unwrap());
+        }
     }
 
-    let body = Body::from(format!("Invalid request path: {}", req.uri().path())); 
-    let res = Response::builder().status(StatusCode::BAD_REQUEST).body(body).unwrap(); 
+    let body = Body::from(format!("Invalid request path: {}", req.uri().path()));
+    let res = Response::builder()
+        .status(StatusCode::BAD_REQUEST)
+        .body(body)
+        .unwrap();
     return Ok(res);
 }
 
+/// This function reads the fastly.toml file and gets the deployed version. This is only run at
+/// compile time. Since we bump the version number after building (during the deploy) we return
+/// the version incremented by one so the version returned will match the deployed version.
+/// courtesy Jim Rainville <jrainville@fastly.com>
+fn get_version() -> i32 {
+    Config::new()
+        .merge(config::File::from_str(
+            include_str!("../fastly.toml"), // assumes the existence of fastly.toml
+            FileFormat::Toml,
+        ))
+        .unwrap()
+        .get_str("version")
+        .unwrap()
+        .parse::<i32>()
+        .unwrap_or(0)
+        + 1
+}
+
+/// initialize the logger
+/// courtesy Jim Rainville <jrainville@fastly.com>
 fn logging_init() {
     log_fastly::Logger::builder()
         .max_level(log::LevelFilter::Debug)
         .default_endpoint(LOGGING_ENDPOINT)
         .init();
     fastly::log::set_panic_endpoint(LOGGING_ENDPOINT).unwrap();
+    log::debug!("*******************************************************");
+    log::debug!("Get Test Version:{}", get_version());
 }
--- src/spacex_tle.rs
+++ src/spacex_tle.rs
@@ -1,10 +1,9 @@
-use fastly::{Request, Body, RequestExt, Response};
-use fastly::http::{Method};
+use fastly::http::{HeaderValue, Method};
 use fastly::request::CacheOverride;
+use fastly::{Body, Error, Request, RequestExt, Response};
+use serde_json::value;
 use std::collections::HashMap;
 use std::time::Instant;
-use serde_json::{value};
-use log;
 
 const BACKEND_SPACEXDATA: &str = "SpaceXData";
 const BACKEND_N2YO: &str = "n2yo";
@@ -13,173 +12,241 @@ const N2YO_URI: &str = "https://api.n2yo.com/rest/v1/satellite/";
 
 #[derive(Debug)]
 pub struct SpacexTLE {
-  n2yo_api_key: &'static str,
-  txn_limit: i32,
-  _txn_count: i32,
+    n2yo_api_key: &'static str,
+    txn_limit: i64,
+    _txn_count: i64,
 }
 
 impl SpacexTLE {
-  pub fn new(n2yo_api_key: &'static str, txn_limit: i32) -> SpacexTLE {
-    return SpacexTLE { n2yo_api_key, txn_limit, _txn_count: 0 };
-  }
-
-  pub fn payload_tles(&mut self, mission_id: &str) -> Option<HashMap<String, Vec<String>>> {
-    let t0 = Instant::now();
-    let norad_ids = self.get_norad_ids_for_mission(mission_id)?;
-    self.log_elapsed(t0, "get NORAD IDs");
-
-    let mut payload_tles = HashMap::new();
-    for (payload_id, norad_ids_for_payload) in &norad_ids {
-      let t0 = Instant::now();
-      let limit = self.txn_limit - self.txn_count();
-      if limit > 0 {
-        let slice = &norad_ids_for_payload[0 as usize .. (limit - 1) as usize];
-        log::debug!("Get TLEs for NORAD IDs {:?}", &slice);
-        let tles = self.get_tles_for_norad_ids(slice);
-        self.log_elapsed(t0, "Get TLEs for NORAD IDs");
-        payload_tles.insert(payload_id.to_owned(), tles);
-      } else {
-        log::info!("TXN limit ({}) reached. Skip TLEs for payload \"{}\"", self.txn_limit, payload_id);
-      }
+    pub fn new(n2yo_api_key: &'static str, txn_limit: i64) -> SpacexTLE {
+        return SpacexTLE {
+            n2yo_api_key,
+            txn_limit,
+            _txn_count: 0,
+        };
     }
-    return Some(payload_tles);
-  }
 
-  fn txn_count(&self) -> i32 {
-    return self._txn_count
-  }
+    pub fn payload_tles(
+        &mut self,
+        mission_id: &str,
+    ) -> Result<Option<HashMap<String, Vec<String>>>, Error> {
+        let t0 = Instant::now();
+        let norad_ids = if let Some(val) = self.get_norad_ids_for_mission(mission_id)? {
+            val
+        } else {
+            return Ok(None);
+        };
 
-  fn log_elapsed(&self, instant: Instant, message: &str) {
-    let elapsed = instant.elapsed();
-    log::info!("Elapsed in {}: {:#?}", message, elapsed)
-  }
+        self.log_elapsed(t0, "get NORAD IDs");
 
-  fn get_norad_ids_for_mission(&mut self, mission_id: &str) -> Option<HashMap<String, Vec<i64>>> {
-    log::debug!("Request NORAD IDs for mission \"{}\"", mission_id);
-    let mut filter = HashMap::new();
-    filter.insert("mission_id", mission_id);
-    filter.insert("filter", "rocket/second_stage/payloads/(payload_id,norad_id)");
-    let response = self.spacexdata_request("launches", filter);
-    if response.is_array() {
-      let launches = response.as_array().unwrap();
-      if launches.len() == 0 {
-        log::debug!("No launches found for mission {}", mission_id);
-        return None;
-      } else {
-        log::debug!("{} launches found for mission {}", launches.len(), mission_id);
-        let mut payload_norad_ids = HashMap::new();
-        for launch in launches {
-          let rocket = launch.get("rocket").unwrap();
-          let second_stage = rocket.get("second_stage").unwrap();
-          let payloads = second_stage.get("payloads").unwrap().as_array().unwrap();
-          log::debug!("{} payloads in mission {}", payloads.len(), mission_id);
-          for payload in payloads {
-            let payload_id = payload.get("payload_id").unwrap().as_str().unwrap().to_owned();
-            let norad_ids = payload.get("norad_id").unwrap().as_array().unwrap();
-            log::debug!("Payload {} has {} NORAD ID(s)", payload_id, norad_ids.len());
-            let norad_ids = norad_ids.iter().map(|x| x.as_i64().unwrap()).collect();
-            payload_norad_ids.insert(payload_id, norad_ids);
-          }
+        let mut payload_tles = HashMap::new();
+        for (payload_id, norad_ids_for_payload) in norad_ids {
+            let t0 = Instant::now();
+            let limit = (self.txn_limit - self.txn_count()) as usize;
+            if limit > 0 {
+                let slice_end = limit.min(norad_ids_for_payload.len());
+                let slice = &norad_ids_for_payload[..slice_end];
+                log::debug!("Get TLEs for NORAD IDs {:?}", &slice);
+                let tles = self.get_tles_for_norad_ids(slice)?;
+                self.log_elapsed(t0, "Get TLEs for NORAD IDs");
+                payload_tles.insert(payload_id.to_owned(), tles);
+            } else {
+                log::info!(
+                    "TXN limit ({}) reached. Skip TLEs for payload \"{}\"",
+                    self.txn_limit,
+                    payload_id
+                );
+            }
         }
-        return Some(payload_norad_ids);
-      }
-    } else {
-      panic!("SpaceXData API did not return an array")
-    }    
-  }
-
-  fn get_tles_for_norad_ids(&mut self, norad_ids: &[i64]) -> Vec<String> {
-    let mut tles = Vec::new();
-    for norad_id in norad_ids {
-      let path = format!("tle/{}", norad_id);
-      let response = self.n2yo_request(&path);
-      let tle_str = response.get("tle").unwrap().as_str().unwrap();
-      if !tle_str.is_empty() {
-          let split: Vec<String> = tle_str.split("\r\n").map(String::from).collect();
-          for tle in split {
-            tles.push(tle);
-          }
-      }
+        return Ok(Some(payload_tles));
     }
-    return tles
-  }
 
-  fn spacexdata_request(&mut self, path: &str, params: HashMap<&str, &str>) -> value::Value {
-    let mut uri: String = SPACEXDATA_URI.to_owned();
-    uri.push_str(path);
-    if params.len() > 0 {
-      uri.push_str("?");
-      let mut count = 0;
-      for (key, value) in params {
-        let kv = format!("{}={}", key, value);
-        uri.push_str(&kv);
-        if count > 0 {
-          uri.push('&');
+    fn txn_count(&self) -> i64 {
+        log::debug!("TXN count: {}", self._txn_count);
+        return self._txn_count;
+    }
+
+    fn log_elapsed(&self, instant: Instant, message: &str) {
+        let elapsed = instant.elapsed();
+        log::info!("Elapsed in {}: {:#?}", message, elapsed)
+    }
+
+    fn get_norad_ids_for_mission(
+        &mut self,
+        mission_id: &str,
+    ) -> Result<Option<HashMap<String, Vec<i64>>>, Error> {
+        log::debug!("Request NORAD IDs for mission \"{}\"", mission_id);
+        let mut filter = HashMap::new();
+        filter.insert("mission_id", mission_id);
+        filter.insert(
+            "filter",
+            "rocket/second_stage/payloads/(payload_id,norad_id)",
+        );
+        let response = self.spacexdata_request("launches", filter)?;
+        if response.is_array() {
+            let launches = response.as_array().unwrap();
+            if launches.len() == 0 {
+                log::debug!("No launches found for mission {}", mission_id);
+                return Ok(None);
+            } else {
+                log::debug!(
+                    "{} launches found for mission {}",
+                    launches.len(),
+                    mission_id
+                );
+                let mut payload_norad_ids = HashMap::new();
+                let mut count = 0;
+                for launch in launches {
+                    count = count + 1;
+                    let rocket = launch.get("rocket").unwrap();
+                    let second_stage = rocket.get("second_stage").unwrap();
+                    let payloads = second_stage.get("payloads").unwrap().as_array().unwrap();
+                    log::debug!(
+                        "{} payloads in mission {}, launch {}",
+                        payloads.len(),
+                        mission_id,
+                        count
+                    );
+                    for payload in payloads {
+                        let payload_id = payload
+                            .get("payload_id")
+                            .unwrap()
+                            .as_str()
+                            .unwrap()
+                            .to_owned();
+                        let norad_ids = payload.get("norad_id").unwrap().as_array().unwrap();
+                        log::debug!("Payload {} has {} NORAD ID(s)", payload_id, norad_ids.len());
+                        let norad_ids = norad_ids.iter().map(|x| x.as_i64().unwrap()).collect();
+                        payload_norad_ids.insert(payload_id, norad_ids);
+                    }
+                }
+                return Ok(Some(payload_norad_ids));
+            }
+        } else {
+            panic!("SpaceXData API did not return an array")
         }
-        count = count + 1;
-      }
     }
-    let response = self.request(Method::GET, &uri, &BACKEND_SPACEXDATA, None, None, None);
-    let content_type = response.headers().get("content-type").unwrap();
-    if content_type.to_str().unwrap().contains("json") {
-      return serde_json::from_str(&response.into_body().into_string()).unwrap();
-    } else {
-      panic!("SpaceXData API did not return JSON");
+
+    fn default_header_value(&self) -> HeaderValue {
+        return HeaderValue::from_str("").unwrap();
     }
-  }
 
- fn n2yo_request(&mut self, path: &str) -> value::Value {
-    let mut uri: String = N2YO_URI.to_owned();
-    uri.push_str(path);
-    uri.push_str("?");
-    uri.push_str(self.n2yo_api_key);
-    let response = self.request(Method::GET, &uri, &BACKEND_N2YO, None, None, None);
-    let content_type = response.headers().get("content-type").unwrap();
-    if content_type.to_str().unwrap().contains("json") {
-      let body_str = response.into_body().into_string().to_owned(); 
-      return serde_json::from_str(&body_str).unwrap();
-    } else {
-      panic!("n2yo API did not return JSON");
+    fn get_tles_for_norad_ids(&mut self, norad_ids: &[i64]) -> Result<Vec<String>, Error> {
+        let mut tles = Vec::new();
+        for norad_id in norad_ids {
+            let path = format!("tle/{}", norad_id);
+            let response = self.n2yo_request(&path)?;
+            let tle_str = response.get("tle").unwrap().as_str().unwrap();
+            if !tle_str.is_empty() {
+                let split: Vec<String> = tle_str.split("\r\n").map(String::from).collect();
+                for tle in split {
+                    tles.push(tle);
+                }
+            }
+        }
+        return Ok(tles);
     }
-  }
 
-  fn request(&mut self, method: Method, uri: &str, backend: &str, headers: Option<HashMap<String, String>>, body: Option<&str>, cache_override: Option<CacheOverride>) -> Response<Body> {
-    let t0 = Instant::now();
-    let mut builder = Request::builder().method(method.as_str()).uri(uri);
-   
-    // Set request headers if passed
-    if headers.is_some() {
-     for (key, value) in &headers.unwrap() {
-        builder = builder.header(key, value);
-      } 
+    fn spacexdata_request(
+        &mut self,
+        path: &str,
+        params: HashMap<&str, &str>,
+    ) -> Result<value::Value, Error> {
+        let mut uri: String = SPACEXDATA_URI.to_owned();
+        uri.push_str(path);
+        if params.len() > 0 {
+            uri.push_str("?");
+            let mut count = 0;
+            for (key, value) in params {
+                if count > 0 {
+                    uri.push('&');
+                }
+                let kv = format!("{}={}", key, value);
+                uri.push_str(&kv);
+                count = count + 1;
+            }
+        }
+        log::debug!("SpaceXData URI: {}", uri);
+        let response = self.request(Method::GET, &uri, &BACKEND_SPACEXDATA, None, None, None)?;
+        let default_header_value = self.default_header_value();
+        let content_type = response
+            .headers()
+            .get("Content-Type")
+            .unwrap_or(&default_header_value)
+            .to_str()
+            .unwrap();
+        if content_type.contains("json") {
+            let body_str = &response.into_body().into_string();
+            return Ok(serde_json::from_str(&body_str).unwrap());
+        } else {
+            panic!("SpaceXData API did not return JSON");
+        }
     }
-    
-    // body is the "terminal state" of the builder, so we have to call body().unwrap to get the Request
-    // object
-    let mut request;
-    match body { 
-      Some(body) => request = builder.body(Body::from(body)).unwrap(),
-      None => request = builder.body(Body::from("")).unwrap(),
-    };
-   
-    cache_override.and_then(|c| Some(*request.cache_override_mut() = c));
-    // Yes, this will panic! on failure, but (at least for now) I want that.
-    let response = request.send(backend).unwrap();
-    let log_str = format!("({}) {} {}", backend, method.as_str(), uri); 
-    self.log_elapsed(t0, &log_str);
-    self.increment_txn_count();
-   
-    // log the response body
-    let (parts, mut body) = response.into_parts();
-    let body_str = String::new();
-    body.write_str(&body_str);
-    let response = Response::from_parts(parts, body);
-    log::debug!("Response body: {}", body_str);
 
-    return response; 
-  }
+    fn n2yo_request(&mut self, path: &str) -> Result<value::Value, Error> {
+        let mut uri: String = N2YO_URI.to_owned();
+        uri.push_str(path);
+        uri.push_str("?apiKey=");
+        uri.push_str(self.n2yo_api_key);
+        let response = self.request(Method::GET, &uri, &BACKEND_N2YO, None, None, None)?;
+        let default_header_value = self.default_header_value();
+        let content_type = response
+            .headers()
+            .get("Content-Type")
+            .unwrap_or(&default_header_value)
+            .to_str()
+            .unwrap();
+        if content_type.contains("json") {
+            let body_str = response.into_body().into_string().to_owned();
+            return Ok(serde_json::from_str(&body_str).unwrap());
+        } else {
+            panic!("n2yo API did not return JSON");
+        }
+    }
 
-  fn increment_txn_count(&mut self) {
-    self._txn_count = self._txn_count + 1;
-  }
+    fn request(
+        &mut self,
+        method: Method,
+        uri: &str,
+        backend: &str,
+        headers: Option<HashMap<String, String>>,
+        body: Option<&str>,
+        cache_override: Option<CacheOverride>,
+    ) -> Result<Response<Body>, Error> {
+        let t0 = Instant::now();
+        log::debug!("[TXN {}] {} {}", self.txn_count(), method, uri);
+        let mut builder = Request::builder().method(method.as_str()).uri(uri);
+
+        // Set request headers if passed
+        if headers.is_some() {
+            for (key, value) in &headers.unwrap() {
+                builder = builder.header(key, value);
+            }
+        }
+
+        // body is the "terminal state" of the builder, so we have to call body().unwrap to get the Request
+        // object
+        let mut request;
+        match body {
+            Some(body) => request = builder.body(Body::from(body)).unwrap(),
+            None => request = builder.body(Body::from("")).unwrap(),
+        };
+
+        cache_override.and_then(|c| Some(*request.cache_override_mut() = c));
+        let response = request.send(backend)?;
+        log::debug!("Request complete");
+
+        let (parts, body) = response.into_parts();
+        let body_str = body.into_string();
+        log::debug!("Response body: {}", body_str);
+        let new_response = Response::from_parts(parts, Body::from(body_str));
+        let log_str = format!("({}) {} {}", backend, method.as_str(), uri);
+        self.log_elapsed(t0, &log_str);
+        self.increment_txn_count();
+        return Ok(new_response);
+    }
+
+    fn increment_txn_count(&mut self) {
+        self._txn_count = self._txn_count + 1;
+    }
 }
